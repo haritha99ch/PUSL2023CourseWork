6ba2e4544f2a702a76d26914d43a286b7c108e21
Haritha Rathnayake <chamararathnayaka@outlook.com>
2022-05-03 19:11:52 +0530

    ER: Query: Post Image

--- app/src/main/java/com/example/login/Data/GGDbContext.kt
+++ app/src/main/java/com/example/login/Data/GGDbContext.kt
@@ -1,20 +1,19 @@
 package com.example.login.Data
 
 import android.content.Context
+import android.provider.MediaStore
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.login.Data.Entities.Account
-import com.example.login.Data.Entities.Comment
-import com.example.login.Data.Entities.Post
-import com.example.login.Data.Entities.User
+import com.example.login.Data.Entities.*
 
 @Database(
     entities = [
         User::class,
         Account::class,
         Post::class,
-        Comment::class
+        Comment::class,
+        Image::class
     ],
     version = 1
 )
--- app/src/main/java/com/example/login/Data/GGdbDao.kt
+++ app/src/main/java/com/example/login/Data/GGdbDao.kt
@@ -2,6 +2,7 @@ package com.example.login.Data
 
 import androidx.room.Dao
 import androidx.room.Query
+import com.example.login.Data.Entities.Image
 import com.example.login.Data.Entities.Relations.*
 
 @Dao
@@ -9,6 +10,9 @@ interface GGdbDao {
 
     @Query("Select * From Comment Where PostId=:postId")
     fun selectCommentswAccount(postId:Int):List<CommentwAccount>
+
+    @Query("Select * From Image Where PostId=:postId")
+    fun selectPostImage(postId: Int):Image
 
     @Query("Select * From Post")
     fun selectPostswAccount():List<PostwAccount>
