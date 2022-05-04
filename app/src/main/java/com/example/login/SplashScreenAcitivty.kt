39c2a0e1163bcba626c0705b6bc69b42d55832b4
InduwaraRBS <induwarakanchana567@gmail.com>
2022-05-04 19:06:26 +0530

    added a splash screen but theres no app logo

---
+++ app/src/main/java/com/example/login/SplashScreenAcitivty.kt
@@ -0,0 +1,26 @@
+package com.example.login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+
+
+class SplashScreenAcitivty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen_acitivty)
+
+        val splash = findViewById<ImageView>(R.id.splash_icon)
+
+            splash.alpha = 0f
+            splash.animate().setDuration(1500).alpha(1f).withEndAction {
+                val i = Intent(this, MainActivity::class.java)
+                startActivity(i)
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                finish()
+            }
+
+    }
+}
