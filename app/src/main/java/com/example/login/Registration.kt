2186b24051f2ce3373d271b7ef9843522edaf001
Haritha Rathnayake <chamararathnayaka@outlook.com>
2022-05-14 16:11:43 +0530

    Account Create values initialized

--- app/src/main/java/com/example/login/Registration.kt
+++ app/src/main/java/com/example/login/Registration.kt
@@ -3,7 +3,8 @@ package com.example.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.util.Log
+import android.widget.*
 
 class Registration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +12,20 @@ class Registration : AppCompatActivity() {
         setContentView(R.layout.activity_registration)
 
         val  signinbutton2 = findViewById<Button>(R.id.signin)
+        var txtEmail=findViewById<EditText>(R.id.email)
+        var txtUserName=findViewById<EditText>(R.id.username)
+        var txtAge=findViewById<EditText>(R.id.age)
+        var rgGender=findViewById<RadioGroup>(R.id.rgGender)
+        var valGender:String
+        rgGender.setOnCheckedChangeListener{rgGender, i ->
+            var rb=findViewById<RadioButton>(i)
+            if (rb!=null){
+                valGender=rb.hint.toString()
+                Log.i("Gender", "$valGender gender")
+            }
+        }
+        var txtPassword=findViewById<EditText>(R.id.password)
+        var txtConfirmpassword=findViewById<EditText>(R.id.confirmpassword)
 
         signinbutton2.setOnClickListener {
             val intent = Intent(this,login::class.java)
